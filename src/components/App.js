5bd875fc98520de29743e63fd3500fe0672c5a36
Christopher Steven <christopherstevennunoo@gmail.com>
2023-08-02 18:54:24 -0700

    three main components

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,32 @@
 import '../styles/App.css';
+import { createBrowserRouter, RouterProvider} from 'react-router-dom';
+import Main from './Main';
+import Result from './Result';
+import Quiz from './Quiz';
+
+//my routes are here
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Main/>,
+  },
+  {
+    path: '/quiz',
+    element: <Quiz/>,
+  },
+  {
+    path: '/result',
+    element: <Result/>,
+  },
+]);
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>heeey</h1>
-    </div>
+    <>
+      <RouterProvider router={router}/>
+    </>
+    
   );
 }
 
